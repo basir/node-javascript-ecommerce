1a75c0db266d1cf3c4ff1f1d9f06bcd764e4407d
Basir <basir.jafarzadeh@gmail.com>
2020-06-30 16:21:48 +0430

    Video-13-Product-Screen

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,14 @@ app.use(cors());
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found!' });
+  }
+});
 
 app.listen(5000, () => {
   console.log('serve at http://localhost:5000');
