cb1958d02ba3d4ee11903fdddd0cb5fd2784ae33
Basir <basir.jafarzadeh@gmail.com>
2020-06-30 19:38:43 +0430

    Video-14-Product-Screen-UI

--- frontend/src/srceens/ProductScreen.js
+++ frontend/src/srceens/ProductScreen.js
@@ -1,5 +1,6 @@
 import { parseRequestUrl } from '../utils';
 import { getProduct } from '../api';
+import Rating from '../components/Rating';
 
 const ProductScreen = {
   render: async () => {
@@ -8,7 +9,56 @@ const ProductScreen = {
     if (product.error) {
       return `<div>${product.error}</div>`;
     }
-    return `<h1>${product.name}</h1>`;
+    return `
+    <div class="content">
+      <div class="back-to-result">
+        <a href="/#/">Back to result </a>
+      </div>
+      <div class="details">
+        <div class="details-image">
+          <img src="${product.image}" alt="${product.name}" />
+        </div>
+        <div class="details-info">
+          <ul>
+            <li>
+              <h1>${product.name}</h1>
+            </li>
+            <li>
+            ${Rating.render({
+              value: product.rating,
+              text: `${product.numReviews} reviews`,
+            })}
+            </li>
+            <li>
+              Price: <strong>$${product.price}</strong>
+            </li>
+            <li>
+              Description:
+              <div>
+                ${product.description}
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="details-action">
+            <ul>
+              <li>
+                Price: $${product.price}
+              </li>
+              <li>
+                Status : 
+                  ${
+                    product.countInStock > 0
+                      ? `<span class="success">In Stock</span>`
+                      : `<span class="error">Unavailable</span>`
+                  }
+              </li>
+              <li>
+                  <button id="add-button" class="fw primary">Add to Cart </div>
+            </ul>
+        </div>
+      </div>
+    </div>`;
   },
 };
 export default ProductScreen;
