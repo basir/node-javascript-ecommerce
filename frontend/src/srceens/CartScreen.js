e9409c16c381629a19bbfc430a8a0cfbbe85a3b6
Basir <basir.jafarzadeh@gmail.com>
2020-07-03 10:59:51 +0430

    Video-17-Cart-Screen-UI

--- frontend/src/srceens/CartScreen.js
+++ frontend/src/srceens/CartScreen.js
@@ -30,8 +30,62 @@ const CartScreen = {
         qty: 1,
       });
     }
-    return `<div>Cart Screen</div>
-    <div>${getCartItems().length}</div>
+    const cartItems = getCartItems();
+    return `
+    <div class="content cart">
+      <div class="cart-list">
+        <ul class="cart-list-container">
+          <li>
+            <h3>Shopping Cart</h3>
+            <div>Price</div>
+          </li>
+          ${
+            cartItems.length === 0
+              ? '<div>Cart is empty. <a href="/#/">Go Shopping</a>'
+              : cartItems
+                  .map(
+                    (item) => `
+            <li>
+              <div class="cart-image">
+                <img src="${item.image}" alt="${item.name}" />
+              </div>
+              <div class="cart-name">
+                <div>
+                  <a href="/#/product/${item.product}">
+                    ${item.name}
+                  </a>
+                </div>
+                <div>
+                  Qty: 
+                  <select class="qty-select" id="${item.product}">
+                    <option value="1">1</option>
+                  </select>
+                  <button type="button" class="delete-button" id="${item.product}">
+                    Delete
+                  </button>
+                </div>
+              </div>
+              <div class="cart-price">
+                $${item.price}
+              </div>
+            </li>
+            `
+                  )
+                  .join('\n')
+          } 
+        </ul>
+      </div>
+      <div class="cart-action">
+          <h3>
+            Subtotal (${cartItems.reduce((a, c) => a + c.qty, 0)} items)
+            :
+            $${cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
+          </h3>
+          <button id="checkout-button" class="primary fw">
+            Proceed to Checkout
+          </button>
+      </div>
+    </div>
     `;
   },
 };
