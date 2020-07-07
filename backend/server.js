80b3ba93aa49d9db0b175c5585bdf1b4f8141cc8
Basir <basir.jafarzadeh@gmail.com>
2020-07-07 17:33:16 +0430

    Video-35-Edit-Product-UI

--- backend/server.js
+++ backend/server.js
@@ -29,17 +29,7 @@ app.use('/api/orders', orderRouter);
 app.get('/api/paypal/clientId', (req, res) => {
   res.send({ clientId: config.PAYPAL_CLIENT_ID });
 });
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found!' });
-  }
-});
+
 app.use((err, req, res, next) => {
   const status = err.name && err.name === 'ValidationError' ? 400 : 500;
   res.status(status).send({ message: err.message });
