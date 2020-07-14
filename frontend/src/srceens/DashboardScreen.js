4423080f26ee5d30e12daeff9ece89440b2e4ff3
Bassir Jafarzadeh (Coding Teacher) <basir.jafarzadeh@gmail.com>
2020-07-14 11:08:43 +0430

    Video-42-Dashboard-Summary (#42)

--- frontend/src/srceens/DashboardScreen.js
+++ frontend/src/srceens/DashboardScreen.js
@@ -1,16 +1,37 @@
 import DashboardMenu from '../components/DashboardMenu';
+import { getSummary } from '../api';
 
+let summary = {};
 const DashboardScreen = {
   after_render: () => {},
-  render: () => {
+  render: async () => {
+    summary = await getSummary();
     return `
     <div class="dashboard">
       ${DashboardMenu.render({ selected: 'dashboard' })}
       <div class="dashboard-content">
         <h1>Dashboard</h1>
-        <div>
-          Info and Charts will be added here
-        </div>
+       
+        <ul class="summary-items">
+          <li>
+            <div class="summary-title color1">
+              <span><i class="fa fa-users"></i> Users</span>
+            </div>
+            <div class="summary-body">${summary.users[0].numUsers}</div>
+          </li>
+          <li>
+            <div class="summary-title color2">
+              <span><i class="fa fa-users"></i> Orders</span>
+            </div>
+            <div class="summary-body">${summary.orders[0].numOrders}</div>
+          </li>
+          <li>
+            <div class="summary-title color3">
+              <span><i class="fa fa-users"></i> Sales</span>
+            </div>
+            <div class="summary-body">$${summary.orders[0].totalSales}</div>
+          </li>
+        </ul>
       </div>
     </div>
     `;
