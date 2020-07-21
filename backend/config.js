c67f7d399079f9f7b2bcad46f2083246d791a426
Bassir Jafarzadeh (Coding Teacher) <basir.jafarzadeh@gmail.com>
2020-07-21 13:03:42 +0430

    Video-44-Publish-Heroku (#44)

--- backend/config.js
+++ backend/config.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
+  PORT: process.env.PORT || 5000,
   MONGODB_URL: process.env.MONGODB_URL,
   JWT_SECRET: process.env.JWT_SECRET,
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
