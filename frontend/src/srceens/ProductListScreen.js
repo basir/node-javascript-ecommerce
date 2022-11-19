e99ca1c2cd883ebe8b547908a58052ef37d812a7
Basir <basir.jafarzadeh@gmail.com>
2022-11-19 10:41:16 +0330

    fix getProducts error in ProductListScreen.js

--- frontend/src/srceens/ProductListScreen.js
+++ frontend/src/srceens/ProductListScreen.js
@@ -1,25 +1,25 @@
-import DashboardMenu from '../components/DashboardMenu';
-import { getProducts, createProduct, deleteProduct } from '../api';
-import { showLoading, hideLoading, rerender, showMessage } from '../utils';
+import DashboardMenu from "../components/DashboardMenu";
+import { getProducts, createProduct, deleteProduct } from "../api";
+import { showLoading, hideLoading, rerender, showMessage } from "../utils";
 
 const ProductListScreen = {
   after_render: () => {
     document
-      .getElementById('create-product-button')
-      .addEventListener('click', async () => {
+      .getElementById("create-product-button")
+      .addEventListener("click", async () => {
         const data = await createProduct();
         document.location.hash = `/product/${data.product._id}/edit`;
       });
-    const editButtons = document.getElementsByClassName('edit-button');
+    const editButtons = document.getElementsByClassName("edit-button");
     Array.from(editButtons).forEach((editButton) => {
-      editButton.addEventListener('click', () => {
+      editButton.addEventListener("click", () => {
         document.location.hash = `/product/${editButton.id}/edit`;
       });
     });
-    const deleteButtons = document.getElementsByClassName('delete-button');
+    const deleteButtons = document.getElementsByClassName("delete-button");
     Array.from(deleteButtons).forEach((deleteButton) => {
-      deleteButton.addEventListener('click', async () => {
-        if (confirm('Are you sure to delete this product?')) {
+      deleteButton.addEventListener("click", async () => {
+        if (confirm("Are you sure to delete this product?")) {
           showLoading();
           const data = await deleteProduct(deleteButton.id);
           if (data.error) {
@@ -33,10 +33,10 @@ const ProductListScreen = {
     });
   },
   render: async () => {
-    const products = await getProducts();
+    const products = await getProducts({});
     return `
     <div class="dashboard">
-    ${DashboardMenu.render({ selected: 'products' })}
+    ${DashboardMenu.render({ selected: "products" })}
     <div class="dashboard-content">
       <h1>Products</h1>
       <button id="create-product-button" class="primary">
@@ -71,7 +71,7 @@ const ProductListScreen = {
             </tr>
             `
               )
-              .join('\n')}
+              .join("\n")}
           </tbody>
         </table>
       </div>
